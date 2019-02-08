c4b289c6883060bff16fe431e8074b53a3fd1f12
Nathan Pillai <natepill@gmail.com>
2019-02-08 00:08:46 -0800

    connection and disconnection examlpe

--- server/server.js
+++ server/server.js
@@ -1,14 +1,39 @@
 const express = require('express')
 const path = require('path');
 const port = process.env.PORT || 5000
-
+const socketIO = require('socket.io');
+const http = require('http');
+//we need to use http ourselves and configure it with express
 
 
 var app = express();
+
+var server = http.createServer(app)
+var io = socketIO(server) //This is our websocket server, how we communicate between server and client
+
+
 const publicPath = path.join(__dirname, '../public/');
 app.use(express.static(publicPath))
 
 
-app.listen(port, () => {
+//lets you register an event listener
+// built in event listener like connection lets you listen when a client connects to the server
+// 'socket' which is passed into the callback represents the individual socket as opposed to all the users connected to the server
+io.on('connection', (socket) => {
+    console.log('Server: new user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User has been disconnected from the server');
+    })
+})
+
+
+app.get('/', (req, res) => {
+    console.log('sda');
+})
+
+
+
+server.listen(port, () => {
     console.log(`Server is up on ${port}`);
 })
