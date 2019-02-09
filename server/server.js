bc7efe7dd43e6e9df0726d7df432948c0c0d427c
Nathan Pillai <natepill@gmail.com>
2019-02-08 19:31:50 -0800

    Create and generate message utility

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 5000
 const socketIO = require('socket.io');
 const http = require('http');
 //we need to use http ourselves and configure it with express
-
+const {generateMessage} = require('./utils/message');
 
 var app = express();
 
@@ -23,35 +23,21 @@ io.on('connection', (socket) => {
     console.log('Server: new user connected');
 
     //Welcome from admin
-    socket.emit('newMessage', {
-        from: "Admin",
-        text: 'welcome to the chat',
-        createdAt: new Date().getTime()
-    })
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-    socket.broadcast.emit('newMessage', {
-        from: "Admin",
-        text: "New User Joined",
-        createdAt: new Date().getTime()
-    })
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined'));
+
 
     //socket.emit is not a listener. Instead of listening to an event, we are creating the event
     //first argument is the event that we are creating, then we send any data back to the client along with the new event
 
-    //listener
-    socket.on('createEmail', (newEmail) => {
-        console.log("createEmail", newEmail);
-    })
+
     //listener
     socket.on('createMessage', function(createdMessage) {
         console.log("created message", createdMessage);
 
-        //socket.emit emits to a single connection, io.emit emits to all connections
-        // io.emit('newMessage', {
-        //     from: createdMessage.from,
-        //     text: createdMessage.text,
-        //     createdAt: new Date().getTime() //may want to use this to sort messages in a chatroom
-        // })
+        // socket.emit emits to a single connection, io.emit emits to all connections
+        io.emit('newMessage', generateMessage(message.from, message.text))
 
         // To broadcast, we need to specify the individual socket. This lets the socket.io library know which users shouldn't get the event
         // Will send event to everyone except this socket
---
+++ server/utils/message.js
@@ -0,0 +1,11 @@
+//Utility functions related to messaging
+
+var generateMessage = (from, text) => {
+    return {
+        from,
+        text,
+        createdAt: new Date().getTime()
+    }
+};
+
+module.exports = {generateMessage}
